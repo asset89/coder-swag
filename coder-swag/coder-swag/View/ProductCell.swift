cce7df1e6183dcc5dc8258980bbcd3f3cb38cf36
asset89 <asset89@gmail.com>
2018-08-31 19:48:04 +0600

    Collection View Cell Built

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by МакЛарен on 31.08.2018.
+//  Copyright © 2018 Asset Ryskul. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
